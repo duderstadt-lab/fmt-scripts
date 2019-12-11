27b160ae862dd1e8dd026ffa26eb71c689e45092
Karl Duderstadt <duderstadt@biochem.mpg.de>
2019-12-11 09:56:13 +0100

    First commit of FMT Pipeline1 script and Add Regions script

---
+++ Add_Regions.groovy
@@ -0,0 +1,28 @@
+#@ MoleculeArchive archive
+
+import de.mpg.biochem.mars.molecule.*;
+import de.mpg.biochem.mars.table.*;
+import de.mpg.biochem.mars.util.*;
+
+MarsImageMetadata metadata = archive.getImageMetadata(0);
+
+//MarsRegion(name, column, start, end, hex color, opacity (0-1))
+
+metadata.addRegion(new MarsRegion("coil20 Positive Peak", "slice", 1135, 1145, "#FFCA28", 0.2))
+metadata.addRegion(new MarsRegion("coil20 Negative Peak", "slice", 535, 545, "#42A5F5", 0.2))
+
+metadata.addRegion(new MarsRegion("coil2p5 Peak", "slice", 4010, 4020, "#FFCA28", 0.2))
+metadata.addRegion(new MarsRegion("coil2p5 Background", "slice", 2700, 2710, "#42A5F5", 0.2))
+
+metadata.addRegion(new MarsRegion("First Reversal RF", "slice", 100, 120, "#FFCA28", 0.2))
+metadata.addRegion(new MarsRegion("First Reversal FF", "slice", 200, 220, "#42A5F5", 0.2))
+
+metadata.addRegion(new MarsRegion("Last Reversal RF", "slice", 14100, 14120, "#42A5F5", 0.2))
+metadata.addRegion(new MarsRegion("Last Reversal FF", "slice", 13960, 13980, "#FFCA28", 0.2))
+
+metadata.addRegion(new MarsRegion("Negative Coiling Slope", "slice", 350, 450, "#66BB6A", 0.2))
+
+metadata.addRegion(new MarsRegion("Before Enzyme", "slice", 4500, 4520, "#42A5F5", 0.2))
+metadata.addRegion(new MarsRegion("After Enzyme", "slice", 7800, 7820, "#FFCA28", 0.2))
+
+archive.putImageMetadata(metadata)
---
+++ FMT_Pipeline1.groovy
@@ -0,0 +1,313 @@
+#@ MoleculeArchive archive
+#@ Double (value=0.05) stuckRevThreshold
+#@ Double (value=0.2) stuckMSDThreshold
+#@ Integer (value=4500) minLength
+#@ Double (value=0.0009) doubleCoilingLowerBound
+#@ Double (value=10) doubleCoilingUpperBound
+#@ Double (value=0.5) coilable20LowerBound
+#@ Double (value=5) coilable20UpperBound
+#@ Double (value=0.2) enzymaticLowerBound
+#@ Double (value=6) enzymaticUpperBound
+#@ Double (value=1) turns_per_second
+#@ Double (value=2) turns_per_cycle
+#@ Integer (value=1) driftZeroRegionStart
+#@ Integer (value=100) driftZeroRegionEnd
+#@ ImageJ ij
+#@ LogService logService
+  
+import de.mpg.biochem.mars.molecule.*;
+import de.mpg.biochem.mars.table.*;
+import de.mpg.biochem.mars.util.*;
+import de.mpg.biochem.mars.molecule.commands.*
+
+archive.lock()
+
+//Start log
+String titleBlock = LogBuilder.buildTitleBlock("FMT Pipeline 1 - Start")
+logService.info(titleBlock)
+archive.addLogMessage(titleBlock)
+
+logger = new LogBuilder()
+logger.addParameter("FMT Pipeline 1 Verion", 1)
+
+//Check to make sure there is only one metadata item
+if (archive.getNumberOfImageMetadataRecords() > 1) {
+	logService.info(logger.buildParameterList())
+	logService.info("More than one MarsImageMetadata item found - aborting.")	
+	logService.info(LogBuilder.endBlock(false))
+
+	archive.addLogMessage(logger.buildParameterList())
+	archive.addLogMessage("More than one MarsImageMetadata item found - aborting.")
+	archive.addLogMessage(LogBuilder.endBlock(false));
+
+	archive.unlock()
+	
+	return;
+}
+
+//Build input parameters
+MarsImageMetadata metadata = archive.getImageMetadata(0);
+
+coil20_Positive_Peak = metadata.getRegion("coil20 Positive Peak")
+coil20_Negative_Peak = metadata.getRegion("coil20 Negative Peak")
+
+coil2p5_Peak = metadata.getRegion("coil2p5 Peak")
+coil2p5_Background = metadata.getRegion("coil2p5 Background")
+
+First_Reversal_RF = metadata.getRegion("First Reversal RF")
+First_Reversal_FF = metadata.getRegion("First Reversal FF")
+
+Last_Reversal_RF = metadata.getRegion("Last Reversal RF")
+Last_Reversal_FF = metadata.getRegion("Last Reversal FF")
+
+Negative_Coiling_Slope = metadata.getRegion("Negative Coiling Slope")
+Positive_Coiling_Slope = metadata.getRegion("Positive Coiling Slope")
+
+Before_Enzyme = metadata.getRegion("Before Enzyme")
+After_Enzyme = metadata.getRegion("After Enzyme")
+
+logger.addParameter("stuckRevThreshold", stuckRevThreshold)
+logger.addParameter("stuckMSDThreshold", stuckMSDThreshold)
+logger.addParameter("minLength", minLength)
+logger.addParameter("doubleCoilingLowerBound", doubleCoilingLowerBound)
+logger.addParameter("doubleCoilingUpperBound", doubleCoilingUpperBound)
+logger.addParameter("enzymaticLowerBound", enzymaticLowerBound)
+logger.addParameter("enzymaticUpperBound", enzymaticUpperBound)
+
+String parameterList = logger.buildParameterList();
+logService.info(parameterList)
+archive.addLogMessage(parameterList)
+
+archive.getMoleculeUIDs().parallelStream().forEach({ UID ->
+	Molecule molecule = archive.get(UID)
+	MarsTable table = molecule.getDataTable()
+      
+	//RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, xColumn, yColumn, regionOne, regionTwo, parameterName)
+    //coil20
+	RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, "slice", "x", coil20_Positive_Peak, coil20_Negative_Peak, "coil20")
+
+	//coil2p5
+	RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, "slice", "x", coil2p5_Peak, coil2p5_Background, "coil2p5")
+
+	//First Reversal
+	RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, "slice", "x", First_Reversal_RF, First_Reversal_FF, "rev_begin_x")
+	RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, "slice", "y", First_Reversal_RF, First_Reversal_FF, "rev_begin_y")
+
+	//Last Reversal
+	RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, "slice", "x", Last_Reversal_RF, Last_Reversal_FF, "rev_end_x")
+	RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, "slice", "y", Last_Reversal_RF, Last_Reversal_FF, "rev_end_y")
+
+	//Enzymatic Activity Detection
+	RegionDifferenceCalculatorCommand.calcRegionDifference(molecule, "slice", "x", Before_Enzyme, After_Enzyme, "enzymatic")
+
+	//MSDCalculatorCommand.calcMSD(molecule, column, parameterName)
+	//MSD
+	MSDCalculatorCommand.calcMSD(molecule, "x", "x_MSD")
+	MSDCalculatorCommand.calcMSD(molecule, "y", "y_MSD")
+
+	//Calculate slopes
+	output = table.linearRegression("slice","x", Negative_Coiling_Slope.getStart(), Negative_Coiling_Slope.getEnd())
+	molecule.setParameter("Slope_Neg_20", output[2])
+
+    //Add Tags
+    //stuckRev
+    if (Math.abs(molecule.getParameter("rev_begin_y")) < stuckRevThreshold &&\
+		Math.abs(molecule.getParameter("rev_end_y")) < stuckRevThreshold &&\
+		Math.abs(molecule.getParameter("rev_begin_x")) < stuckRevThreshold &&\
+		Math.abs(molecule.getParameter("rev_end_x")) < stuckRevThreshold) {
+			molecule.addTag("stuckRev")
+		}
+
+	//stuckMSD
+	if (molecule.getParameter("x_MSD") < stuckMSDThreshold &&\
+		molecule.getParameter("y_MSD") < stuckMSDThreshold) {
+			molecule.addTag("stuckMSD")
+	}
+
+	//coilable20
+	if (molecule.getParameter("coil20") > coilable20LowerBound &&\
+		molecule.getParameter("coil20") < coilable20UpperBound) {
+			molecule.addTag("coilable20")
+	}
+
+	//doubleCoiling
+	if (molecule.getParameter("Slope_Neg_20") > doubleCoilingLowerBound &&\
+		molecule.getParameter("Slope_Neg_20") < doubleCoilingUpperBound) {
+			molecule.addTag("doubleCoiling")
+	}
+
+	//shortTrajectory
+	int deadSlice = table.getValue("slice",table.getRowCount()-1)
+	if (deadSlice < minLength)
+		molecule.addTag("shortTrajectory")
+
+	//singleTether
+	if(molecule.hasTag("coilable20") &&\
+	  !molecule.hasTag("doubleCoiling") &&\
+	  !molecule.hasTag("shortTrajectory")) {
+	  	molecule.addTag("singleTether")
+	}
+
+	//enzymatic
+	if (molecule.hasTag("singleTether") &&\
+		molecule.getParameter("enzymatic") > enzymaticLowerBound &&\
+		molecule.getParameter("enzymatic") < enzymaticUpperBound) {
+			molecule.addTag("chi")
+	}
+
+	archive.put(molecule)
+})
+
+//Finish Log
+logService.info(LogBuilder.endBlock())
+archive.addLogMessage(LogBuilder.endBlock())
+archive.unlock()
+
+sleep(1000)
+
+//Drift Calculator
+final DriftCalculatorCommand driftCalc = new DriftCalculatorCommand();
+driftCalc.setContext(ij.getContext());
+
+//Set all the input parameters
+driftCalc.setArchive(archive);
+driftCalc.setBackgroundTag("stuckMSD");
+driftCalc.setInputX("x");
+driftCalc.setInputY("y");
+driftCalc.setOutputX("x_drift");
+driftCalc.setOutputY("y_drift");
+driftCalc.setMode("mean")
+driftCalc.setUseIncompleteTraces(false);
+
+driftCalc.run();
+
+sleep(1000)
+
+//Drift Corrector
+final DriftCorrectorCommand driftCorr = new DriftCorrectorCommand();
+driftCorr.setContext(ij.getContext())
+
+//Set all the input parameters
+driftCorr.setArchive(archive)
+driftCorr.setFromSlice(driftZeroRegionStart)
+driftCorr.setToSlice(driftZeroRegionEnd)
+driftCorr.setMetaX("x_drift")
+driftCorr.setMetaY("y_drift")
+driftCorr.setInputX("x")
+driftCorr.setInputY("y")
+driftCorr.setOutputX("x_drift_corr")
+driftCorr.setOutputY("y_drift_corr")
+
+driftCorr.run()
+
+sleep(1000)
+
+archive.lock()
+
+String titleBlock2 = LogBuilder.buildTitleBlock("FMT Pipeline 1 - End")
+logService.info(titleBlock2)
+archive.addLogMessage(titleBlock2)
+
+logger2 = new LogBuilder()
+logger2.addParameter("turns_per_second", turns_per_second)
+logger2.addParameter("turns_per_cycle", turns_per_cycle)
+logger2.addParameter("driftZeroRegionStart", driftZeroRegionStart)
+logger2.addParameter("driftZeroRegionEnd", driftZeroRegionEnd)
+
+String parameterList2 = logger2.buildParameterList();
+logService.info(parameterList2)
+archive.addLogMessage(parameterList2)
+
+//Calculate poscycles and negcycles on a molecules-by-molecule basis
+
+archive.getMoleculeUIDs().parallelStream().forEach({ UID ->
+      Molecule molecule = archive.get(UID)
+      MarsTable table = molecule.getDataTable()
+
+	  double[] pos_coils_per_second = table.linearRegression("Time (s)", "x_drift_corr", Positive_Coiling_Slope.getStart(), Positive_Coiling_Slope.getEnd())
+
+	  molecule.setParameter("pos_coil_slope", pos_coils_per_second[2])
+
+	  	if (!table.hasColumn("poscycles"))
+			table.appendColumn("poscycles")
+
+		for (int row = 0; row < table.getRowCount(); row++) {
+			double conversion = (-1)*(turns_per_second/pos_coils_per_second[2])/turns_per_cycle
+			table.setValue("poscycles", row, conversion*table.getValue("x_drift_corr", row))
+		}
+
+	  double[] neg_coils_per_second = table.linearRegression("Time (s)", "x_drift_corr", Negative_Coiling_Slope.getStart(), Negative_Coiling_Slope.getEnd())
+
+	  molecule.setParameter("neg_coil_slope", neg_coils_per_second[2])
+
+	  	if (!table.hasColumn("negcycles"))
+			table.appendColumn("negcycles")
+	
+		for (int row = 0; row < table.getRowCount(); row++) {
+			double conversion = (1)*(turns_per_second/neg_coils_per_second[2])/turns_per_cycle
+			table.setValue("negcycles", row, conversion*table.getValue("x_drift_corr", row))
+		}
+
+      archive.put(molecule)
+})
+
+//Calculate global median values for pos_coil_slope and neg_coil_slope
+
+posCoilTable = new MarsTable("PosCoilRates","poscoil")
+negCoilTable = new MarsTable("NegCoilRates","negcoil")
+
+archive.getMoleculeUIDs().stream()\
+.filter{ UID -> archive.moleculeHasTag(UID, "singleTether")}\
+//.filter{ UID -> archive.moleculeHasTag(UID, "coilable2p5")}\
+.forEach{ UID ->
+     Molecule molecule = archive.get(UID)
+
+     double pSlope = molecule.getParameter("pos_coil_slope")
+     double nSlope = molecule.getParameter("neg_coil_slope")
+
+	if (pSlope != Double.NaN) {
+		posCoilTable.appendRow()
+		posCoilTable.setValue("poscoil",posCoilTable.getRowCount()-1, pSlope)
+	}
+
+	if (nSlope != Double.NaN) {
+	    negCoilTable.appendRow()
+		negCoilTable.setValue("negcoil",negCoilTable.getRowCount()-1, nSlope)
+	}
+}
+
+posCoilGlobal = posCoilTable.median("poscoil")
+negCoilGlobal = negCoilTable.median("negcoil")
+
+println("median poscoil slope global " + posCoilGlobal)
+println("median negcoil slope global " + negCoilGlobal)
+
+//Use global pos_coil_slope and neg_coil_slope to calculate poscycles and negcycles based on global averages.
+
+archive.getMoleculeUIDs().parallelStream().forEach({ UID ->
+	      Molecule molecule = archive.get(UID)
+	      MarsTable table = molecule.getDataTable()
+
+		  	if (!table.hasColumn("negcyclesG"))
+				table.appendColumn("negcyclesG")
+
+			for (int row = 0; row < table.getRowCount(); row++) {
+				double conversion = (1)*(turns_per_second/negCoilGlobal)/turns_per_cycle
+				table.setValue("negcyclesG", row, conversion*table.getValue("x_drift_corr", row))
+			}
+
+			if (!table.hasColumn("poscyclesG"))
+				table.appendColumn("poscyclesG")
+
+			for (int row = 0; row < table.getRowCount(); row++) {
+				double conversion = (-1)*(turns_per_second/posCoilGlobal)/turns_per_cycle
+				table.setValue("poscyclesG", row, conversion*table.getValue("x_drift_corr", row))
+			}
+
+	      archive.put(molecule)
+	})
+	
+logService.info(LogBuilder.endBlock(true))
+archive.addLogMessage(LogBuilder.endBlock(true))
+   
+archive.unlock()
